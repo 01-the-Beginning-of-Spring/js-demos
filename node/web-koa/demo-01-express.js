1a6a9d8c304175a1cbd7c6966ea9035116c7acb6
JacobCN <coder211@163.com>
2019-05-14 10:27:33 +0800

    update

---
+++ node/web-koa/demo-01-express.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+app.get('/test', function (req, res) {
+    fs.readFile('/file1', function (err, data) {
+        if (err) {
+            res.status(500).send('read file1 error');
+        }
+        fs.readFile('/file2', function (err, data) {
+            if (err) {
+                res.status(500).send('read file2 error');
+            }
+            res.type('text/plain');
+            res.send(data);
+        });
+    });
+});
