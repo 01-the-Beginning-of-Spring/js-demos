11cb34212b6c174cfa0548fccef7e2fc02097dab
JacobCN <coder211@163.com>
2019-05-18 16:33:46 +0800

    update

--- node/web-express/sample-1.js
+++ node/web-express/sample-1.js
@@ -2,7 +2,6 @@
 
 var express = require('express');
 var fs = require('fs');
-
 var app = express();
 
 app.get('/', function (req, res) {
@@ -13,17 +12,20 @@ app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
 
+var file1 = 'data/data.txt';
+var file2 = file1;
 app.get('/test', function (req, res) {
-    fs.readFile('/file1', function (err, data) {
+    fs.readFile(file1, function (err, data1) {
         if (err) {
-            res.status(500).send('read file1 error');
+            res.status(500).send('read \'' + file1 + '\' error');
         }
-        fs.readFile('/file2', function (err, data) {
+        // res.send(data1);
+        fs.readFile(file2, function (err, data2) {
             if (err) {
-                res.status(500).send('read file2 error');
+                res.status(500).send('read \''+ file2 + '\' error');
             }
             res.type('text/plain');
-            res.send(data);
+            res.send(data1 + data2);
         });
     });
 });
